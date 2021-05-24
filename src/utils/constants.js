f1a19c324d7241a1b76ebc72b7ead5ab0afefcf1
Guilherme Luchesi <guilherme.luchesi@dattos.com.br>
2021-05-24 11:43:58 -0300

    feat: visual improvements for report message

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
 module.exports = {
   CUSTOM_TIMER_ID: 'custom-timmer',
+  WRAPPER_SELECTOR: '.NzPR9b',
   INITIAL_TIMER_IN_MINUTES: 20,
 };
--- src/utils/report.js
+++ src/utils/report.js
@@ -1,4 +1,4 @@
-const { INITIAL_TIMER_IN_MINUTES } = require('./constants');
+const { INITIAL_TIMER_IN_MINUTES, WRAPPER_SELECTOR } = require('./constants');
 const normalizeTime = require('./normalizeTime');
 
 const report = (timer) => {
@@ -9,10 +9,46 @@ const report = (timer) => {
     : INITIAL_TIMER_IN_MINUTES - minutes;
   const normalizedMinutes = normalizeTime(minutesSpent);
 
-  const secondsSpent = normalizeTime(60 - seconds);
+  const secondsSpent = 60 - seconds;
   const normalizedSeconds = normalizeTime(secondsSpent);
 
-  console.log(`Tempo gasto: ${normalizedMinutes}:${normalizedSeconds}`);
+  const alertMessage = document.createElement('div');
+  const reportMessage = `Tempo gasto: ${normalizedMinutes}:${normalizedSeconds}`;
+
+  alertMessage.innerText = reportMessage;
+  alertMessage.style.position = 'absolute';
+  alertMessage.style.bottom = '-50px';
+  alertMessage.style.backgroundColor = 'white';
+  alertMessage.style.width = '100%';
+  alertMessage.style.padding = '10px 15px';
+  alertMessage.style.borderTopLeftRadius = '8px';
+  alertMessage.style.borderBottomLeftRadius = '8px';
+  alertMessage.style.transform = 'translateX(100%)';
+  alertMessage.style.transition = 'transform 0.3s ease-in-out';
+  alertMessage.style.position = 'absolute';
+  alertMessage.style.fontSize = '0.9rem';
+  alertMessage.style.fontWeight = '500';
+  alertMessage.style.color = '#5f6368';
+  alertMessage.style.boxShadow = '0 1px 2px 0 rgb(60 64 67 / 30%), 0 2px 6px 2px rgb(60 64 67 / 15%)';
+
+  const wrapper = document.querySelector(WRAPPER_SELECTOR);
+  if (wrapper) {
+    wrapper.appendChild(alertMessage);
+
+    setTimeout(() => {
+      alertMessage.style.transform = 'translateX(0)';
+    }, 100);
+
+    setTimeout(() => {
+      alertMessage.style.transform = 'translateX(100%)';
+
+      setTimeout(() => {
+        alertMessage.remove();
+      }, 500);
+    }, 5000);
+  } else {
+    console.log(reportMessage);
+  }
 };
 
 module.exports = report;
